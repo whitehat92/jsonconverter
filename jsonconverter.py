8567b6bf788ab7245fbe122c651409a02b1562b3
whitehat92 <ze_correia93@hotmail.com>
2019-10-09 17:39:54 +0100

    Update jsonconverter.py

--- jsonconverter.py
+++ jsonconverter.py
@@ -1,11 +1,10 @@
 import sys
 import json
+import demjson
 file = sys.argv[1]
 
 with open(file, "r") as filetoread:
     maindata = filetoread.read()
     mainobjct = maindata[maindata.find("{") : maindata.rfind("}")+1]
     objectjson = json.dumps(mainobjct)
-    if "{" in objectjson and "}" in objectjson:
-        print(objectjson.replace("{", "{"+"\n"),objectjson.replace("}","\n"+"}"))
-
+    print(demjson.decode(objectjson))
